17806d584bc61e070c7d4f13db940d0a3508766b
Ana Chaves <87650149+anachavesv8@users.noreply.github.com>
2022-03-04 15:52:04 -0300

    Evolulçao do jogo de adivinhação com while.

--- JogodeAdivinhacao.py
+++ JogodeAdivinhacao.py
@@ -4,14 +4,20 @@
 from random import randint
 from time import sleep
 
-computador = randint(0, 5) #faz o computador sortear o numero
+computador = randint(0, 10) #faz o computador sortear o numero
 print('-+-' * 20)
-print('Vou pensar em um número entre 0 a 5. Tente adivinhar...')
+print('Vou pensar em um número entre 0 a 10. Tente adivinhar...')
 print('-=-' * 20)
-jogador = int(input('Em que número eu pensei? ')) #jogador tenta adivinhar
-print('PROCESSANDO...')
-sleep(3)
-if jogador == computador:
-    print('PARABENS!! Você conseguiu me vencer!')
-else:
-    print('GANHEI!!! Eu pensei no número {} e não no {}'.format(computador, jogador))
+acertou = False
+palpite = 0
+while not acertou:
+    jogador = int(input('\nEm que número eu pensei? ')) #jogador tenta adivinhar
+    palpite += 1
+    if jogador == computador:
+       acertou = True
+    else:
+        if jogador < computador:
+            print('Mais...Tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez.')
+print('\nACERTOU!!! com {} tentativas'.format(palpite))
